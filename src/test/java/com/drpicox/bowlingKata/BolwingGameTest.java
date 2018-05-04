4c0c4006dbd7723dde2ead6ae0e2bda45095ab41
David Rodenas <david.rodenas@gmail.com>
2018-05-04 08:17:16 +0200

    initial commit

---
+++ src/test/java/com/drpicox/bowlingKata/BolwingGameTest.java
@@ -0,0 +1,14 @@
+package com.drpicox.bowlingKata;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.sql.SQLException;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.assertThat;
+
+public class BowlingGameTest {
+
+
+}
