7e682ea84c07fdd5148161b73c15cb03dfb10bf4
sergillull <sllull@edu.tecnocampus.cat>
2018-05-08 09:56:33 +0200

    slide 14

--- src/main/java/com/drpicox/bowlingKata/Game.java
+++ src/main/java/com/drpicox/bowlingKata/Game.java
@@ -1,5 +1,5 @@
 package com.drpicox.bowlingKata;
 
-public class Main {
+public class Game {
 
 }
--- src/test/java/com/drpicox/bowlingKata/BowlingGameTest.java
+++ src/test/java/com/drpicox/bowlingKata/BowlingGameTest.java
@@ -2,12 +2,16 @@ package com.drpicox.bowlingKata;
 
 import org.junit.Before;
 import org.junit.Test;
-
 import java.sql.SQLException;
-
 import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.assertThat;
+import junit.framework.TestCase;
+import com.drpicox.bowlingKata.Game;
 
-public class BowlingGameTest {
+public class BowlingGameTest extends TestCase {
 
+    @Test
+    public void testGutterGame() throws Exception {
+        Game g = new Game();
+    }
 }
