329ffb2bb14cc48bedfc1fe8c115b18f86aedf8d
sergillull <sllull@edu.tecnocampus.cat>
2018-05-10 11:58:22 +0200

    slide 33

--- src/test/java/com/drpicox/bowlingKata/BowlingGameTest.java
+++ src/test/java/com/drpicox/bowlingKata/BowlingGameTest.java
@@ -31,4 +31,12 @@ public class BowlingGameTest extends TestCase {
             g.roll(1);
         assertEquals(20,g.score());
     }
+
+//    public void testOneSpare() throws Exception{
+//        g.roll(5);
+//        g.roll(5); //spare
+//        g.roll(3);
+//        rollMany(17,0);
+//        assertEquals(16,g.score());
+//    }
 }
