6b28777994b24ac36b53de69c78b3b91741bd62a
sergillull <sllull@edu.tecnocampus.cat>
2018-05-10 09:02:52 +0200

    slide 23

--- src/main/java/com/drpicox/bowlingKata/Game.java
+++ src/main/java/com/drpicox/bowlingKata/Game.java
@@ -1,9 +1,12 @@
 package com.drpicox.bowlingKata;
 
 public class Game {
+    private int score = 0;
+
     public void roll (int pins){
+        score += pins;
     }
     public int score() {
-        return 0;
+        return score;
     }
 }
